5c444ac65dca510b2f3cd019801abc5a4d96b202
Mahmoud Alaa <asticalaa1@gmail.com>
2024-10-18 08:31:17 +0300

    Refactor package.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,9 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
   },
-  devtool: "source-map",
+  devtool: "inline-source-map",
   module: {
     rules: [{ test: /\.ts$/, use: "ts-loader", exclude: /node_modules/ }],
   },
